07681de8070291848e9a258967a2db3b1153f3aa
Kerry M. Soileau <kerry@kerrysoileau.com>
2017-04-12 10:52:39 -0500

    Fixed code for r dot v.

--- TrackingSat/TrackingSat/src/trackingsat/test.java
+++ TrackingSat/TrackingSat/src/trackingsat/test.java
@@ -1,46 +1,62 @@
 package trackingsat;
 
+class Package
+{
+	double rdR;
+	double rdT;
+	double rdotv;
+	double vdR;
+	double vdT;
+	double vdv;
+	
+	public String toString()
+	{
+		return "Package [rdR=" + rdR + ", rdT=" + rdT + ", rdotv=" + rdotv
+				+ ", vdR=" + vdR + ", vdT=" + vdT + ", vdv=" + vdv + "]";
+	}
+}
+
 public class test
 {
+	public static Package doit(Vector rv,Vector vv,Vector Ru,Vector Tu,Vector Hu,Measurement meas)
+	{
+		Package ret=new Package();
+		ret.rdotv=rv.dot(vv);
+		double r=rv.norm();
+		double v=vv.norm();
+		ret.rdR=rdotR.eval(meas, r);
+		ret.rdT=rdotT.eval(meas,r,ret.rdotv);
+		ret.vdR=vdotR.eval(meas, r, ret.rdotv);
+		ret.vdT=vdotT.eval(meas, r, ret.rdotv,v);
+		ret.vdv=vdotv.eval(meas, r, ret.rdotv);
+		
+		return ret;
+	}
+	
+	public static Package doit(double r,double v,Vector Ru,Vector Tu,Vector Hu,Measurement meas)
+	{
+		Package ret=new Package();
+		ret.rdotv=trackingsat.rdotv.eval(meas, r, v);
+		ret.rdR=rdotR.eval(meas, r);
+		ret.rdT=rdotT.eval(meas,r,ret.rdotv);
+		ret.vdR=vdotR.eval(meas, r, ret.rdotv);
+		ret.vdT=vdotT.eval(meas, r, ret.rdotv,v);
+		ret.vdv=vdotv.eval(meas, r, ret.rdotv);
+		
+		return ret;
+	}
+	
 	public static void main(String[] args)
 	{
-		Vector rv=new Vector(.2,.4,.6);
-		Vector vv=new Vector(-.4,.2,0.);
-		double v=vv.norm();
+		Vector rv=new Vector(.2,.4,.65);
+		Vector vv=new Vector(-.4,.2,0.1);
 		Vector Ru=new Vector(0.5531092473661592, 0.5014195097738353, 0.6653184468329488);
 		Vector Tu=new Vector(-0.14758384350290915, 0.844939121319505, -0.5140981330453669);
 		Vector Hu=new Vector(-0.8199324177119414, 0.18616217790407796, 0.5413450599230224);
 		Measurement meas=new Measurement(rv,vv,Ru,Tu);
 		System.out.println("meas="+meas);
 		
-		double rdotv=rv.dot(vv);
-		System.out.println("rdotv="+rdotv);
-		double r=rv.norm();
-		System.out.println("r="+r);
-		//rdR test
-		double rdR=rdotR.eval(meas, r);
-		System.out.println("rdR="+rdR);
-		double rdRactual=rv.dot(Ru);
-		System.out.println("rdRactual="+rdRactual);
-		//rdT test
-		double rdT=rdotT.eval(meas,r,rdotv);
-		System.out.println("rdT="+rdT);
-		double rdTactual=rv.dot(Tu);
-		System.out.println("rdTactual="+rdTactual);
-		//vdR test
-		double vdR=vdotR.eval(meas, r, rdotv);
-		System.out.println("vdR="+vdR);
-		double vdRactual=vv.dot(Ru);
-		System.out.println("vdRactual="+vdRactual);
-		//vdT test
-		double vdT=vdotT.eval(meas, r, rdotv,v);
-		System.out.println("vdT="+vdT);
-		double vdTactual=vv.dot(Tu);
-		System.out.println("vdTactual="+vdTactual);
-		//vdv test
-		double vdv=vdotv.eval(meas, r, rdotv);
-		System.out.println("vdv="+vdv);
-		double vdvactual=vv.dot(vv);
-		System.out.println("vdvactual="+vdvactual);
+		Package dt=doit(rv.norm(),vv.norm(),Ru,Tu,Hu,meas);
+		System.out.println(dt);
 	}
 }
--- TrackingSat/TrackingSat/src/trackingsat/vdotT.java
+++ TrackingSat/TrackingSat/src/trackingsat/vdotT.java
@@ -8,7 +8,8 @@ public class vdotT
 	{
 		m0=m.m0;
 		m2=m.m2;
-		return (1 - Measurement.power(r,2) + Measurement.power(r,5) - m0*(1 + Measurement.power(r,3)) + Measurement.power(r,3)*(1 - m2 + 2*Measurement.power(v,2)))/(4.*Measurement.power(r,3));
+		return (1 - Measurement.power(r,2) + Measurement.power(r,5) - m0*(1 + Measurement.power(r,3)) 
+				+ Measurement.power(r,3)*(1 - m2 + 2*Measurement.power(v,2)))/(4.*Measurement.power(r,3));
 	}
 }
 
